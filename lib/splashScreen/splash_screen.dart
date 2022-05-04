0e8efeee146fa06b1ecddc5520592d143ccbfb80
chaoticgoodpanda <lwinm@seas.upenn.edu>
2022-05-04 13:04:59 +0200

    Wired up overlapping sellers_app packages  and class files

---
+++ lib/splashScreen/splash_screen.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:riders_app/global/global.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  startTimer()
+  {
+    Timer(const Duration(seconds: 3), () async {
+      // need to create instance of our current firebase user
+      // if user is already logged in
+      if(firebaseAuth.currentUser != null) {
+        // logged in, push to homepage
+        // Navigator.push(context, MaterialPageRoute(builder: (c) => const HomeScreen()));
+      }
+      else {
+        // if not logged in, go to login screen
+        // Navigator.push(context, MaterialPageRoute(builder: (c) => const AuthScreen()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset("images/logo.png"),
+              ),
+              const SizedBox(height: 10,),
+              const Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Text(
+                  "Deliver Online Food Orders!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 40,
+                    fontFamily: "Signatra",
+                    letterSpacing: 3,
+                  ),
+                ),
+              )
+            ],
+          )
+        )
+      )
+    );
+  }
+}
